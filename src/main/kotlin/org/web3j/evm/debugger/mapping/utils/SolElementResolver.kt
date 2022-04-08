d0a57184b9e072af055c5782a22299b138a73a45
gtebrean <george@web3labs.com>
2022-04-08 21:27:51 +0300

    add variables to debugger window

--- src/main/kotlin/org/web3j/evm/debugger/mapping/utils/SolElementResolver.kt
+++ src/main/kotlin/org/web3j/evm/debugger/mapping/utils/SolElementResolver.kt
@@ -6,11 +6,12 @@ import me.serce.solidity.lang.psi.SolParameterList
 import me.serce.solidity.lang.psi.impl.SolAssignmentExpressionImpl
 import me.serce.solidity.lang.psi.impl.SolConstructorDefinitionImpl
 import me.serce.solidity.lang.psi.impl.SolContractDefinitionImpl
+import me.serce.solidity.lang.psi.impl.SolNumberTypeImpl
 import me.serce.solidity.lang.psi.impl.SolVarLiteralImpl
+import me.serce.solidity.lang.psi.impl.SolVariableDeclarationImpl
 import me.serce.solidity.lang.types.type
 import org.web3j.evm.debugger.frame.SolidityStackFrame
 import org.web3j.evm.debugger.frame.SolidityNamedValue
-
 
 fun resolveContext(psiElement: SolElement, stackFrame: SolidityStackFrame): String {
     when (psiElement) {
@@ -21,6 +22,10 @@ fun resolveContext(psiElement: SolElement, stackFrame: SolidityStackFrame): Stri
         is SolContractDefinitionImpl -> {
             resolveContract(psiElement, stackFrame)
             return "SolContractDefinitionImpl"
+        }
+        is SolNumberTypeImpl -> {
+            resolveVariable(psiElement, stackFrame)
+            return "SolVariableDeclarationImpl"
         }
         is SolVarLiteralImpl -> {
             resolveLiteral(psiElement, stackFrame)
@@ -57,6 +62,20 @@ private fun resolveLiteral(psiElement: SolElement, stackFrame: SolidityStackFram
                 maybeLiteral.expressionList[0].text,
                 maybeLiteral.expressionList[0].type.toString(),
                 maybeLiteral.expressionList[1].text
+            )
+        )
+    }
+}
+
+
+private fun resolveVariable(psiElement: SolElement, stackFrame: SolidityStackFrame) {
+    val maybeLiteral = PsiTreeUtil.getParentOfType(psiElement, SolVariableDeclarationImpl::class.java)
+    if (maybeLiteral != null) {
+        stackFrame.addValue(
+            SolidityNamedValue(
+                maybeLiteral.firstChild.nextSibling.nextSibling.text,
+                maybeLiteral.firstChild.text,
+                maybeLiteral.parent.lastChild.text
             )
         )
     }
